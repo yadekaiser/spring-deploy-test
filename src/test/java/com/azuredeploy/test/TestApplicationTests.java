157a4b0560dbafe0af1507526ce988ccf7e21ab3
yadekaiser <yadekaiser.dev@outlook.com>
2023-02-08 22:33:53 +0100

    init

---
+++ src/test/java/com/azuredeploy/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azuredeploy.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
